012e5ce9db00f255474093c8bbf92725246ab434
SHIKADA Satoshi <satoc.skd@gmail.com>
2020-02-01 13:46:57 +0900

    バンドルするファイルを分割（node_modulesとアプリ）した

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, './dest'),
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
     },
 
     devServer: {
@@ -49,5 +49,23 @@ module.exports = {
     plugins: [
           new VueLoaderPlugin(),
           new CopyPlugin([{ from: './public' }]),
-    ]
-};
+    ],
+
+    // https://qiita.com/soarflat/items/1b5aa7163c087a91877d
+    optimization: {
+        splitChunks: {
+          // cacheGroups内にバンドルの設定を複数記述できる
+          cacheGroups: {
+            // 今回はvendorだが、任意の名前で問題ない
+            vendor: {
+              // node_modules配下のモジュールをバンドル対象とする
+              test: /node_modules/,
+              name: 'vendor',
+              chunks: 'initial',
+              enforce: true
+            }
+          }
+        }
+      }
+    
+    };

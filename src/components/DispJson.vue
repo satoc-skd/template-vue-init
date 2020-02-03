607afd60c9b5994bedc1b8ebcb58693b9aaf2c01
SHIKADA Satoshi <satoc.skd@gmail.com>
2020-02-03 21:40:22 +0900

    1)data.jsonを名鉄瀬戸線に差し替え 2)停車駅種別のラジオボタンを設ける

--- src/components/DispJson.vue
+++ src/components/DispJson.vue
@@ -5,6 +5,16 @@ export default {
             type: Array,
             required: true,
         },
+    },
+    data () {
+        return {
+            selectedType: 1,
+            typeOptions: [
+                { text: '普通停車', value: '1' },
+                { text: '準急停車', value: '2' },
+                { text: '急行停車', value: '4' },
+            ],
+        }
     }
 }
 </script>
@@ -12,6 +22,16 @@ export default {
 <template>
     <div>
         <h5>引数[arg1]の中身↓</h5>
-        <p>{{ this.arg1 }}</p>
+        <b-form-group label="停車駅">
+            <b-form-radio-group
+                v-model="selectedType"
+                name="some-radios"
+                :options="typeOptions"
+                buttons
+                button-variant="primary"
+            ></b-form-radio-group>
+        </b-form-group>
+        <p v-for="i1 in arg1" :key="i1.key" v-show="(i1.Type & selectedType) > 0"
+        >{{ i1.StationName }}({{ i1.StationNumber }})</p>
     </div>
 </template>
